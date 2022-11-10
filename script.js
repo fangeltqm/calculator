8b4f8f6ec5be843d783b21116c6b41b36da22d44
fangel <fangeltung@gmail.com>
2022-11-10 09:06:40 -0800

    add add, subtract, multiply, and divide functions in js

---
+++ script.js
@@ -0,0 +1,19 @@
+let plus = function(a,b) {
+    return a+b;
+}
+console.log(plus(1,4));
+
+let minus = function(a,b) {
+    return a-b;
+}
+console.log(minus(4,1));
+
+let times = function(a,b) {
+    return a*b;
+}
+console.log(times(4,5));
+
+let divide = function(a,b) {
+    return a/b;
+}
+console.log(divide(30,3));
