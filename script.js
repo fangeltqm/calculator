bda917dbceb7270b1257a0f3420f622fb00fe18a
fangel <fangeltung@gmail.com>
2022-11-10 09:36:44 -0800

    add operate function that can call add, subtract, multiply, divide functions

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ let divide = function(a,b) {
     return a/b;
 }
 console.log(divide(30,3));
+
+
+let operate = function(operator,a,b) {
+    if (operator === '+') {
+        return plus(a,b);
+
+    } else if (operator === '-') {
+        return minus(a,b);
+
+    } else if (operator === '*') {
+        return times(a,b);
+
+    } else if (operator === '/') {
+        return divide(a,b);
+    }
+}
+
+console.log(operate('+',2,4));
+console.log(operate('-',6,2));
+console.log(operate('*',3,4));
+console.log(operate('/',8,4));
