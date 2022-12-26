4531088f6895ca0985b051e01e4b90bd3d0aa334
fangel <fangeltung@gmail.com>
2022-12-25 16:30:05 -0800

    redo javascript

--- script.js
+++ script.js
@@ -1,9 +1,99 @@
+const clear = document.querySelector("#clear");
+const equals = document.querySelector("#equals");
+const decimal = document.querySelector("#decimal");
+
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+
+let display = document.querySelector("#display");
+
+let firstNum = '';
+let secondNum = '';
+let operator = '';
+let solution = '';
+
+numbers.forEach((number) => {
+    number.addEventListener('click', getNumber)
+})
+
+operators.forEach((operator) => {
+    operator.addEventListener('click', getOperator)
+})
+
+clear.addEventListener('click', reset)
+equals.addEventListener('click', calculate)
+decimal.addEventListener('click', appendDecimal)
+
+function getNumber(e) {
+    if(firstNum.length < 9) {
+        firstNum += `${e.target.id}`
+        display.textContent = firstNum;
+   
+        if(secondNum != '') {
+            calculate();
+            display.textContent = firstNum;
+        }
+    }
+}
+
+function getOperator(e) {
+    if (operator === '/' && firstNum == '0') {
+        display.textContent = 'ERROR';
+        return;
+    } 
+    operator = e.target.id;
+    secondNum = firstNum;
+    firstNum = '';
+    
+    if(solution != '') {
+        secondNum = solution;
+        display.textContent = secondNum;
+    }
+}
+
+function reset() {
+    firstNum = '';
+    secondNum = '';
+    operator = '';
+    solution = '';
+    display.textContent = '';
+}
+
+function calculate () {
+    if (operator === '/' && firstNum === '0') {
+        display.textContent = 'ERROR';
+        return;
+    }
+    
+    firstNum = Number(firstNum);
+    secondNum = Number(secondNum);
+    solution = (operate(operator, firstNum, secondNum));
+    firstNum = firstNum.toString();
+    secondNum = secondNum.toString();
+    display.textContent = solution;
+    
+}
+
+function appendDecimal () {
+    if (firstNum.includes(".")) return;
+    if (firstNum === '') {
+        firstNum = '0'
+    }
+    firstNum += '.'
+    display.textContent = firstNum;
+
+
+}
+
+
+
+
 let plus = function(a,b) {
     return a+b;
 }
 
 let minus = function(a,b) {
-    return a-b;
+    return b-a;
 }
 
 let times = function(a,b) {
@@ -11,23 +101,27 @@ let times = function(a,b) {
 }
 
 let divide = function(a,b) {
-    return a/b;
+    return b/a;
 }
 
-let operate = function(operator,a,b) {
+let operate = function(operator, firstNum, secondNum) {
     if (operator === '+') {
-        return plus(a,b);
+        return plus(firstNum,secondNum);
 
     } else if (operator === '-') {
-        return minus(a,b);
+        return minus(firstNum,secondNum);
 
     } else if (operator === '*') {
-        return times(a,b);
+        return times(firstNum,secondNum);
 
     } else if (operator === '/') {
-        return divide(a,b);
+        return divide(firstNum,secondNum);
     }
 }
+
+/* redoing my javascript above. below is old non working
+
+
 
 let display = document.getElementById('display');
 let a = '';
@@ -102,13 +196,14 @@ function getB(e) {
     operatorButtons.forEach((operatorButton) => {
         operatorButton.addEventListener('click', operateAgain)
     });
-    /*
+     
+    -----
     if (resetCalc = true) {
         operatorButtons.forEach((operatorButton) => {
             operatorButton.removeEventListener('click', operateAgain)
         });
     }
-    */
+    -----
 }
 
 const equals = document.getElementById('equals');
@@ -136,3 +231,5 @@ function allClear() {
     });
 
 }
+
+*/
